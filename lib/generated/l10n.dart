c66ac0ab89f320f876b4f3915bffb128470d42bb
Xavi Montero <oxmonteroc83@est.ucacue.edu.ec>
2021-01-02 22:20:17 -0500

    Fix #10

--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -35,61 +35,381 @@ class S {
     return Localizations.of<S>(context, S);
   }
 
-  /// `Welcome \nThe Tambo`
+  /// `Binevenido a \nEl Tambo`
   String get welcomeTambo {
     return Intl.message(
-      'Welcome \nThe Tambo',
+      'Binevenido a \nEl Tambo',
       name: 'welcomeTambo',
       desc: '',
       args: [],
     );
   }
 
-  /// `Kirk The Tambo`
+  /// `Localizacion`
   String get nameKirk {
     return Intl.message(
-      'Kirk The Tambo',
+      'Localizacion',
       name: 'nameKirk',
       desc: '',
       args: [],
     );
   }
 
-  /// `$Free/All day`
+  /// `$Gratis/Todo el día`
   String get priceKirk {
     return Intl.message(
-      '\$Free/All day',
+      '\$Gratis/Todo el día',
       name: 'priceKirk',
       desc: '',
       args: [],
     );
   }
 
-  /// `Center, Tambo`
+  /// `Centro, Tambo`
   String get locationKirk {
     return Intl.message(
-      'Center, Tambo',
+      'Centro, Tambo',
       name: 'locationKirk',
       desc: '',
       args: [],
     );
   }
 
-  /// `According to the existing baptismal books to date, the parish was already founded before the first Baptism inscription, which dates from January 17, 1836, but thanks to historical references from Father Manuel J. Guambaña it can be known that before the 5th of July 1835 was already founded as a suffragan of Jatuncañar. \n\nAs a fact of great importance within the parish life was the erection of the cave dedicated to Jesús del Gran Poder, located in the northern part of the Canton`
+  /// `Según los libros bautismales existentes hasta la fecha, la parroquia estuvo ya fundada antes de la primera inscripción de Bautizo que data del 17 de enero de 1836, pero gracias a referencias históricas del P. Manuel J. Guambaña se puede conocer que antes del 5 de julio de 1835 ya estuvo fundada como sufragánea de Jatuncañar. \n\nComo un hecho de gran importancia dentro de la vida parroquial fue la erección de la gruta dedicada a Jesús del Gran Poder, ubicada en la parte norte del Cantón`
   String get detailsKirk {
     return Intl.message(
-      'According to the existing baptismal books to date, the parish was already founded before the first Baptism inscription, which dates from January 17, 1836, but thanks to historical references from Father Manuel J. Guambaña it can be known that before the 5th of July 1835 was already founded as a suffragan of Jatuncañar. \n\nAs a fact of great importance within the parish life was the erection of the cave dedicated to Jesús del Gran Poder, located in the northern part of the Canton',
+      'Según los libros bautismales existentes hasta la fecha, la parroquia estuvo ya fundada antes de la primera inscripción de Bautizo que data del 17 de enero de 1836, pero gracias a referencias históricas del P. Manuel J. Guambaña se puede conocer que antes del 5 de julio de 1835 ya estuvo fundada como sufragánea de Jatuncañar. \n\nComo un hecho de gran importancia dentro de la vida parroquial fue la erección de la gruta dedicada a Jesús del Gran Poder, ubicada en la parte norte del Cantón',
       name: 'detailsKirk',
       desc: '',
       args: [],
     );
   }
 
-  /// `Nope`
+  /// `No`
   String get urlKirk {
     return Intl.message(
-      'Nope',
+      'No',
       name: 'urlKirk',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Cultura Cañari`
+  String get nameCanari {
+    return Intl.message(
+      'Cultura Cañari',
+      name: 'nameCanari',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `$Free/All day`
+  String get priceCanari {
+    return Intl.message(
+      '\$Free/All day',
+      name: 'priceCanari',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Center, Tambo`
+  String get locationCanari {
+    return Intl.message(
+      'Center, Tambo',
+      name: 'locationCanari',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `According to the existing baptismal books to date, the parish was already founded before the first Baptism inscription, which dates from January 17, 1836, but thanks to historical references from Father Manuel J. Guambaña it can be known that before the 5th of July 1835 was already founded as a suffragan of Jatuncañar. \n\nAs a fact of great importance within the parish life was the erection of the cave dedicated to Jesús del Gran Poder, located in the northern part of the Canton`
+  String get detailsCanari {
+    return Intl.message(
+      'According to the existing baptismal books to date, the parish was already founded before the first Baptism inscription, which dates from January 17, 1836, but thanks to historical references from Father Manuel J. Guambaña it can be known that before the 5th of July 1835 was already founded as a suffragan of Jatuncañar. \n\nAs a fact of great importance within the parish life was the erection of the cave dedicated to Jesús del Gran Poder, located in the northern part of the Canton',
+      name: 'detailsCanari',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Nope`
+  String get urlCanari {
+    return Intl.message(
+      'Nope',
+      name: 'urlCanari',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Vestimenta tipica`
+  String get nameVestimenta {
+    return Intl.message(
+      'Vestimenta tipica',
+      name: 'nameVestimenta',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `$Free/All day`
+  String get pricevestimenta {
+    return Intl.message(
+      '\$Free/All day',
+      name: 'pricevestimenta',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Center, Tambo`
+  String get locationVestimenta {
+    return Intl.message(
+      'Center, Tambo',
+      name: 'locationVestimenta',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `The word cañarí etymologically comes from Kan = snake and from Ara = Guacamaya. \n\nFor  some  linguists,  it  means  descendant  of  the  snake  and  the  macaw.  The  truth  is  that  these  animals  were  considered  sacred.  In  addition  to  venerating  the  hills,  the  moon,  and marbled stones, the Cañarí culture wors-hiped  the  many  lagoons  that  exist  in  the  region.`
+  String get detailsvestimenta {
+    return Intl.message(
+      'The word cañarí etymologically comes from Kan = snake and from Ara = Guacamaya. \n\nFor  some  linguists,  it  means  descendant  of  the  snake  and  the  macaw.  The  truth  is  that  these  animals  were  considered  sacred.  In  addition  to  venerating  the  hills,  the  moon,  and marbled stones, the Cañarí culture wors-hiped  the  many  lagoons  that  exist  in  the  region.',
+      name: 'detailsvestimenta',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Nope`
+  String get urlvestimenta {
+    return Intl.message(
+      'Nope',
+      name: 'urlvestimenta',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Artesanias`
+  String get nameArtesanias {
+    return Intl.message(
+      'Artesanias',
+      name: 'nameArtesanias',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `$Free/All day`
+  String get priceArtesanias {
+    return Intl.message(
+      '\$Free/All day',
+      name: 'priceArtesanias',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Center, Tambo`
+  String get locationArtesanias {
+    return Intl.message(
+      'Center, Tambo',
+      name: 'locationArtesanias',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `The word cañarí etymologically comes from Kan = snake and from Ara = Guacamaya. \n\nFor  some  linguists,  it  means  descendant  of  the  snake  and  the  macaw.  The  truth  is  that  these  animals  were  considered  sacred.  In  addition  to  venerating  the  hills,  the  moon,  and marbled stones, the Cañarí culture wors-hiped  the  many  lagoons  that  exist  in  the  region.`
+  String get detailsArtesanias {
+    return Intl.message(
+      'The word cañarí etymologically comes from Kan = snake and from Ara = Guacamaya. \n\nFor  some  linguists,  it  means  descendant  of  the  snake  and  the  macaw.  The  truth  is  that  these  animals  were  considered  sacred.  In  addition  to  venerating  the  hills,  the  moon,  and marbled stones, the Cañarí culture wors-hiped  the  many  lagoons  that  exist  in  the  region.',
+      name: 'detailsArtesanias',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Nope`
+  String get urlArtesanias {
+    return Intl.message(
+      'Nope',
+      name: 'urlArtesanias',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Gastronomia`
+  String get nameGastronomia {
+    return Intl.message(
+      'Gastronomia',
+      name: 'nameGastronomia',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `$Free/All day`
+  String get priceGastronomia {
+    return Intl.message(
+      '\$Free/All day',
+      name: 'priceGastronomia',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Center, Tambo`
+  String get locationGastronomia {
+    return Intl.message(
+      'Center, Tambo',
+      name: 'locationGastronomia',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `The word cañarí etymologically comes from Kan = snake and from Ara = Guacamaya. \n\nFor  some  linguists,  it  means  descendant  of  the  snake  and  the  macaw.  The  truth  is  that  these  animals  were  considered  sacred.  In  addition  to  venerating  the  hills,  the  moon,  and marbled stones, the Cañarí culture wors-hiped  the  many  lagoons  that  exist  in  the  region.`
+  String get detailsGastronomia {
+    return Intl.message(
+      'The word cañarí etymologically comes from Kan = snake and from Ara = Guacamaya. \n\nFor  some  linguists,  it  means  descendant  of  the  snake  and  the  macaw.  The  truth  is  that  these  animals  were  considered  sacred.  In  addition  to  venerating  the  hills,  the  moon,  and marbled stones, the Cañarí culture wors-hiped  the  many  lagoons  that  exist  in  the  region.',
+      name: 'detailsGastronomia',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Nope`
+  String get urlGastronomia {
+    return Intl.message(
+      'Nope',
+      name: 'urlGastronomia',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `$Free/All day`
+  String get priceMedicina {
+    return Intl.message(
+      '\$Free/All day',
+      name: 'priceMedicina',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Center, Tambo`
+  String get locationMedicina {
+    return Intl.message(
+      'Center, Tambo',
+      name: 'locationMedicina',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `The word cañarí etymologically comes from Kan = snake and from Ara = Guacamaya. \n\nFor  some  linguists,  it  means  descendant  of  the  snake  and  the  macaw.  The  truth  is  that  these  animals  were  considered  sacred.  In  addition  to  venerating  the  hills,  the  moon,  and marbled stones, the Cañarí culture wors-hiped  the  many  lagoons  that  exist  in  the  region.`
+  String get detailsMedicina {
+    return Intl.message(
+      'The word cañarí etymologically comes from Kan = snake and from Ara = Guacamaya. \n\nFor  some  linguists,  it  means  descendant  of  the  snake  and  the  macaw.  The  truth  is  that  these  animals  were  considered  sacred.  In  addition  to  venerating  the  hills,  the  moon,  and marbled stones, the Cañarí culture wors-hiped  the  many  lagoons  that  exist  in  the  region.',
+      name: 'detailsMedicina',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Nope`
+  String get urlMedicina {
+    return Intl.message(
+      'Nope',
+      name: 'urlMedicina',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Killa Fashion`
+  String get nameKilla {
+    return Intl.message(
+      'Killa Fashion',
+      name: 'nameKilla',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `$Free/All day`
+  String get priceKilla {
+    return Intl.message(
+      '\$Free/All day',
+      name: 'priceKilla',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Center, Tambo`
+  String get locationKilla {
+    return Intl.message(
+      'Center, Tambo',
+      name: 'locationKilla',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `The word cañarí etymologically comes from Kan = snake and from Ara = Guacamaya. \n\nFor  some  linguists,  it  means  descendant  of  the  snake  and  the  macaw.  The  truth  is  that  these  animals  were  considered  sacred.  In  addition  to  venerating  the  hills,  the  moon,  and marbled stones, the Cañarí culture wors-hiped  the  many  lagoons  that  exist  in  the  region.`
+  String get detailsKilla {
+    return Intl.message(
+      'The word cañarí etymologically comes from Kan = snake and from Ara = Guacamaya. \n\nFor  some  linguists,  it  means  descendant  of  the  snake  and  the  macaw.  The  truth  is  that  these  animals  were  considered  sacred.  In  addition  to  venerating  the  hills,  the  moon,  and marbled stones, the Cañarí culture wors-hiped  the  many  lagoons  that  exist  in  the  region.',
+      name: 'detailsKilla',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Nope`
+  String get urlKilla {
+    return Intl.message(
+      'Nope',
+      name: 'urlKilla',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Killa Fashion`
+  String get detailsLeyenda {
+    return Intl.message(
+      'Killa Fashion',
+      name: 'detailsLeyenda',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Killa Fashion`
+  String get nameMedicina {
+    return Intl.message(
+      'Killa Fashion',
+      name: 'nameMedicina',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Killa Fashion`
+  String get nameLeyenda {
+    return Intl.message(
+      'Killa Fashion',
+      name: 'nameLeyenda',
       desc: '',
       args: [],
     );
--- lib/util/places.dart
+++ lib/util/places.dart
@@ -1,7 +1,3 @@
-
-
-
-
 import 'package:flutter/material.dart';
 import 'package:tambo/generated/l10n.dart';
 
@@ -11,7 +7,7 @@ class Place {
     return [
   {
     "name": S.of(context).nameKirk,
-    "img": "assets/1.jpeg",
+    "img": "assets/mapa.png",
     "price": S.of(context).priceKirk,
     "location": S.of(context).locationKirk,
     "details": S.of(context).detailsKirk,
@@ -27,66 +23,125 @@ class Place {
     "url":S.of(context).urlKirk
   },
   {
-    "name": "Beach Mauris Blandit",
-    "img": "assets/2.jpeg",
+    "name": S.of(context).nameCanari,
+    "img": "assets/culturas.jpg",
     "price": r"$100/night",
     "location": "Lisbon, Portugal",
-    "details": "Pellentesque in ipsum id orci porta dapibus. "
-        "Nulla porttitor accumsan tincidunt. Donec rutrum "
-        "congue leo eget malesuada. "
-        "\nPraesent sapien massa, convallis a pellentesque "
-        "nec, egestas non nisi. Donec rutrum congue leo eget malesuada. "
-        "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. "
-        "Sed porttitor lectus nibh. Donec sollicitudin molestie malesuada. "
-        "\nCurabitur arcu erat, accumsan id imperdiet et, porttitor at sem. "
-        "Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.",
+    "details": S.of(context).detailsCanari,
+    "imgs":[
+      {
+        "img": "assets/1.jpeg",
+      },{
+        "img": "assets/2.jpeg",
+      },{
+        "img": "assets/3.jpeg",
+      }
+    ],
+    "url":S.of(context).urlKirk
+
   },
   {
-    "name": "Ipsum Restaurant",
-    "img": "assets/3.jpeg",
+    "name": S.of(context).nameVestimenta,
+    "img": "assets/vestimenta.jpg",
     "price": r"$100/night",
     "location": "Paris, France",
-    "details": "Pellentesque in ipsum id orci porta dapibus. "
-        "Nulla porttitor accumsan tincidunt. Donec rutrum "
-        "congue leo eget malesuada. "
-        "\n\nPraesent sapien massa, convallis a pellentesque "
-        "nec, egestas non nisi. Donec rutrum congue leo eget malesuada. "
-        "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. "
-        "Sed porttitor lectus nibh. Donec sollicitudin molestie malesuada. "
-        "\nCurabitur arcu erat, accumsan id imperdiet et, porttitor at sem. "
-        "Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.",
+    "details": S.of(context).detailsvestimenta,
+    "imgs":[
+      {
+        "img": "assets/1.jpeg",
+      },{
+        "img": "assets/2.jpeg",
+      },{
+        "img": "assets/3.jpeg",
+      }
+    ],
+    "url":S.of(context).urlKirk
   },
   {
-    "name": "Curabitur Beach",
-    "img": "assets/4.jpeg",
+    "name": S.of(context).nameArtesanias,
+    "img": "assets/manualidades.jpg",
     "price": r"$100/night",
     "location": "Rome, Italy",
-    "details": "Pellentesque in ipsum id orci porta dapibus. "
-        "Nulla porttitor accumsan tincidunt. Donec rutrum "
-        "congue leo eget malesuada. "
-        "\nPraesent sapien massa, convallis a pellentesque "
-        "nec, egestas non nisi. Donec rutrum congue leo eget malesuada. "
-        "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. "
-        "Sed porttitor lectus nibh. Donec sollicitudin molestie malesuada. "
-        "\nCurabitur arcu erat, accumsan id imperdiet et, porttitor at sem. "
-        "Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.",
+    "details": S.of(context).detailsArtesanias,
+    "imgs":[
+      {
+        "img": "assets/1.jpeg",
+      },{
+        "img": "assets/2.jpeg",
+      },{
+        "img": "assets/3.jpeg",
+      }
+    ],
+    "url":S.of(context).urlKirk
   },
   {
-    "name": "Tincidunt Pool",
-    "img": "assets/5.jpeg",
+    "name": S.of(context).nameGastronomia,
+    "img": "assets/gastronomia.jpg",
     "price": r"$100/night",
     "location": "Madrid, Spain",
-    "details": "Pellentesque in ipsum id orci porta dapibus. "
-        "Nulla porttitor accumsan tincidunt. Donec rutrum "
-        "congue leo eget malesuada. "
-        "\nPraesent sapien massa, convallis a pellentesque "
-        "nec, egestas non nisi. Donec rutrum congue leo eget malesuada. "
-        "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. "
-        "Sed porttitor lectus nibh. Donec sollicitudin molestie malesuada. "
-        "\nCurabitur arcu erat, accumsan id imperdiet et, porttitor at sem. "
-        "Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.",
+    "details": S.of(context).detailsGastronomia,
+    "imgs":[
+      {
+        "img": "assets/1.jpeg",
+      },{
+        "img": "assets/2.jpeg",
+      },{
+        "img": "assets/3.jpeg",
+      }
+    ],
+    "url":S.of(context).urlKirk
   },
+      {
+        "name": S.of(context).nameLeyenda,
+        "img": "assets/leyendas.jpg",
+        "price": S.of(context).priceKirk,
+        "location": S.of(context).locationKirk,
+        "details": S.of(context).detailsLeyenda,
+        "imgs":[
+          {
+            "img": "assets/1.jpeg",
+          },{
+            "img": "assets/2.jpeg",
+          },{
+            "img": "assets/3.jpeg",
+          }
+        ],
+        "url":S.of(context).urlKirk
+      },
+      {
+        "name": S.of(context).nameMedicina,
+        "img": "assets/medicina.jpg",
+        "price": S.of(context).priceKirk,
+        "location": S.of(context).locationKirk,
+        "details": S.of(context).detailsMedicina,
+        "imgs":[
+          {
+            "img": "assets/1.jpeg",
+          },{
+            "img": "assets/2.jpeg",
+          },{
+            "img": "assets/3.jpeg",
+          }
+        ],
+        "url":S.of(context).urlKirk
+      },
+      {
+        "name": S.of(context).nameKilla,
+        "img": "assets/killa.jpg",
+        "price": S.of(context).priceKirk,
+        "location": S.of(context).locationKirk,
+        "details": S.of(context).detailsKilla,
+        "imgs":[
+          {
+            "img": "assets/1.jpeg",
+          },{
+            "img": "assets/2.jpeg",
+          },{
+            "img": "assets/3.jpeg",
+          }
+        ],
+        "url":S.of(context).urlKirk
+      }
 ];
   }
 }
- 
